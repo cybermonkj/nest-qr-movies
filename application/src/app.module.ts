a284e5714a988bebff40eef5764ac5caac5ebdca
Joshua Anop <rolexy.inc@gmail.com>
2023-01-27 19:46:42 +0000

    fixed the import Module errors

--- application/src/app.module.ts
+++ application/src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesModule } from './movies/movies.module';
+import { MoviesService } from './movies/MoviesService';
+import { ListMoviesComponent } from './movies/listMovies.Component';
 
 
 
 
 @Module({
-  imports: [MoviesModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MoviesService, ListMoviesComponent],
 })
 
 export class AppModule {}
